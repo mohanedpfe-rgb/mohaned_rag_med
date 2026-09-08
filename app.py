087192fe30bd2fb29bee06d7f1efd4016a3f675a
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-08 23:25:49 +0100

    use single guarded production runtime bootstrap

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-from rag_project.runtime_hardening import install as install_runtime_hardening
-from rag_project.runtime_hardening_extra import install as install_runtime_hardening_extra
-from rag_project.runtime_recovery import install as install_runtime_recovery
-from rag_project.runtime_quality_gate import install as install_runtime_quality_gate
-from rag_project.runtime_final_gate import install as install_runtime_final_gate
+from rag_project.runtime import install as install_runtime
 
-install_runtime_hardening()
-install_runtime_hardening_extra()
-install_runtime_recovery()
-install_runtime_quality_gate()
-install_runtime_final_gate()
+install_runtime()
 
 from rag_project.app.dev_ui import main
 
