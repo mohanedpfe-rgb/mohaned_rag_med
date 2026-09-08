80275acef7b7bf9ed2423ddac4433a22bda4ed14
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-08 23:21:14 +0100

    install final runtime quality gate in application startup

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from rag_project.runtime_hardening import install as install_runtime_hardening
 from rag_project.runtime_hardening_extra import install as install_runtime_hardening_extra
 from rag_project.runtime_recovery import install as install_runtime_recovery
+from rag_project.runtime_quality_gate import install as install_runtime_quality_gate
 
 install_runtime_hardening()
 install_runtime_hardening_extra()
 install_runtime_recovery()
+install_runtime_quality_gate()
 
 from rag_project.app.dev_ui import main
 
