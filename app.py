31c5aa509101f9224515bd532c12d64e22827d78
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-08 23:14:45 +0100

    wire semantic failure recovery before UI startup

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from rag_project.runtime_hardening import install as install_runtime_hardening
 from rag_project.runtime_hardening_extra import install as install_runtime_hardening_extra
+from rag_project.runtime_recovery import install as install_runtime_recovery
 
 install_runtime_hardening()
 install_runtime_hardening_extra()
+install_runtime_recovery()
 
 from rag_project.app.dev_ui import main
 
