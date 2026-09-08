a80c6a80c77dbf4d77d326bb315919fc7ab3c24f
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-08 23:06:50 +0100

    wire all runtime hardening patches at application startup

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from rag_project.runtime_hardening import install
+from rag_project.runtime_hardening import install as install_runtime_hardening
+from rag_project.runtime_hardening_extra import install as install_runtime_hardening_extra
 
-install()
+install_runtime_hardening()
+install_runtime_hardening_extra()
 
 from rag_project.app.dev_ui import main
 
