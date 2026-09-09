59fbed0048bf09bda67c77b0ccb25660b48b3a77
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-10 00:37:04 +0100

    refactor: activate canonical vector-store hardening

--- rag_project/storage/__init__.py
+++ rag_project/storage/__init__.py
@@ -1,1 +1,7 @@
-"""Storage package."""
+"""Canonical storage package with one runtime hardening layer."""
+
+from rag_project.storage.vector_store_runtime import install as install_vector_store_runtime
+
+install_vector_store_runtime()
+
+__all__ = ["install_vector_store_runtime"]
