73690baf07d20cd82bad5a0a33b6d40b8fba170f
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-10 00:38:19 +0100

    refactor: activate canonical embedding runtime hardening

--- rag_project/embeddings/__init__.py
+++ rag_project/embeddings/__init__.py
@@ -1,5 +1,13 @@
 """Embedding package."""
 
 from .embedding_service import EmbeddingIdentity, EmbeddingProfile, EmbeddingService
+from .embedding_runtime import install as install_embedding_runtime
 
-__all__ = ["EmbeddingIdentity", "EmbeddingProfile", "EmbeddingService"]
+install_embedding_runtime()
+
+__all__ = [
+    "EmbeddingIdentity",
+    "EmbeddingProfile",
+    "EmbeddingService",
+    "install_embedding_runtime",
+]
