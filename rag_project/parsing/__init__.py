97ea571371ab0001d32e88ae971d2a27912035d5
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-11 18:43:10 +0100

    Bind universal PDF extractor

--- rag_project/parsing/__init__.py
+++ rag_project/parsing/__init__.py
@@ -1,1 +1,6 @@
-"""Parsing package."""
+"""Parsing package with universal PDF extraction binding."""
+
+from rag_project.parsing import pdf_extractor as _pdf_extractor_module
+from rag_project.parsing.universal_pdf_extractor import UniversalPDFExtractor
+
+_pdf_extractor_module.PDFExtractor = UniversalPDFExtractor
