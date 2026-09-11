50c7711ff4aebd152795fe67e69a3680a1be6c91
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-11 22:41:44 +0100

    fix: apply final runtime contract layer last

--- rag_project/runtime.py
+++ rag_project/runtime.py
@@ -39,6 +39,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
     from rag_project.runtime_final_contracts_v5 import install as runtime_final_contracts_v5
     from rag_project.runtime_final_contracts_v6 import install as runtime_final_contracts_v6
     from rag_project.runtime_final_contracts_v7 import install as runtime_final_contracts_v7
+    from rag_project.runtime_final_contracts_v8 import install as runtime_final_contracts_v8
 
     return (
         vector_store,
@@ -70,6 +71,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
         runtime_final_contracts_v5,
         runtime_final_contracts_v6,
         runtime_final_contracts_v7,
+        runtime_final_contracts_v8,
     )
 
 
