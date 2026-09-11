0445e9e8c38182080fbfbe824413327825f43169
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-11 18:40:13 +0100

    Inspect storage package

--- rag_project/storage/__init__.py
+++ rag_project/storage/__init__.py
@@ -1,7 +1,3 @@
-"""Canonical storage package with one runtime hardening layer."""
+from rag_project.storage.vector_store import IndexCompatibilityError, VectorStore
 
-from rag_project.storage.vector_store_runtime import install as install_vector_store_runtime
-
-install_vector_store_runtime()
-
-__all__ = ["install_vector_store_runtime"]
+__all__ = ["IndexCompatibilityError", "VectorStore"]
