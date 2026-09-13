2301238b2b2c69bec79c0bee94a11047bcfe3528
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-13 12:51:21 +0100

    purge: remove broken legacy runtime v9 installer

--- rag_project/runtime.py
+++ rag_project/runtime.py
@@ -41,7 +41,6 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
     from rag_project.runtime_final_contracts_v6 import install as runtime_final_contracts_v6
     from rag_project.runtime_final_contracts_v7 import install as runtime_final_contracts_v7
     from rag_project.runtime_final_contracts_v8 import install as runtime_final_contracts_v8
-    from rag_project.runtime_final_contracts_v9 import install as runtime_final_contracts_v9
     from rag_project.runtime_chroma_metadata_fix import install as chroma_metadata_fix
     from rag_project.runtime_answer_recovery_contract import install as answer_recovery_contract
     from rag_project.runtime_deep_contract_fix import install as deep_contract_fix
@@ -86,7 +85,6 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
         runtime_final_contracts_v6,
         runtime_final_contracts_v7,
         runtime_final_contracts_v8,
-        runtime_final_contracts_v9,
         chroma_metadata_fix,
         answer_recovery_contract,
         deep_contract_fix,
