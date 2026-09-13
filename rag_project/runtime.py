980811a8afab0f88f5c3fe53006eead4d7995e5e
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-13 14:49:05 +0100

    fix: install diagnostic contract hardening after runtime stack

--- rag_project/runtime.py
+++ rag_project/runtime.py
@@ -52,6 +52,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
     from rag_project.runtime_functionality_comparison_template_fix import install as functionality_comparison_template_fix
     from rag_project.runtime_functionality_numeric_range_fix import install as functionality_numeric_range_fix
     from rag_project.runtime_functionality_final_audit_fix import install as functionality_final_audit_fix
+    from rag_project.runtime_diagnostic_contract_fix import install as diagnostic_contract_fix
 
     return (
         vector_store,
@@ -95,6 +96,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
         functionality_comparison_template_fix,
         functionality_numeric_range_fix,
         functionality_final_audit_fix,
+        diagnostic_contract_fix,
     )
 
 
