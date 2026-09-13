62612741758d3cda33aa15a1eb1033c0943f17d2
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-13 18:35:22 +0100

    fix: activate Chroma metadata normalization at storage bootstrap

--- rag_project/storage/__init__.py
+++ rag_project/storage/__init__.py
@@ -1,7 +1,9 @@
 """Canonical storage package with one runtime hardening layer."""
 
 from rag_project.storage.vector_store_runtime import install as install_vector_store_runtime
+from rag_project.runtime_chroma_metadata_fix import install as install_chroma_metadata_fix
 
 install_vector_store_runtime()
+install_chroma_metadata_fix()
 
-__all__ = ["install_vector_store_runtime"]
+__all__ = ["install_vector_store_runtime", "install_chroma_metadata_fix"]
