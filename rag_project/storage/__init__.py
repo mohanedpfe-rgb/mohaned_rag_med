7c7e4be9610fc792d3c184594c3c18218cbfc0a1
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-14 15:05:13 +0100

    Wire lexical parity reconciliation hardening

--- rag_project/storage/__init__.py
+++ rag_project/storage/__init__.py
@@ -2,8 +2,14 @@
 
 from rag_project.storage.vector_store_runtime import install as install_vector_store_runtime
 from rag_project.runtime_chroma_metadata_fix import install as install_chroma_metadata_fix
+from rag_project.storage.reconcile_parity_fix import install as install_reconcile_parity_fix
 
 install_vector_store_runtime()
 install_chroma_metadata_fix()
+install_reconcile_parity_fix()
 
-__all__ = ["install_vector_store_runtime", "install_chroma_metadata_fix"]
+__all__ = [
+    "install_vector_store_runtime",
+    "install_chroma_metadata_fix",
+    "install_reconcile_parity_fix",
+]
