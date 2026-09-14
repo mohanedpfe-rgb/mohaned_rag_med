20a73fe86321a57e3217eae7a57f9f896de9a55c
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-14 13:36:10 +0100

    Install terminal READY monotonicity guard

--- rag_project/runtime.py
+++ rag_project/runtime.py
@@ -110,6 +110,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
     from rag_project.runtime_failed_publication_cleanup import install as failed_publication_cleanup
     from rag_project.runtime_post_index_publication_contract import install as post_index_publication_contract
     from rag_project.runtime_post_index_publication_contract_v2 import install as post_index_publication_contract_v2
+    from rag_project.runtime_terminal_state_guard import install as terminal_state_guard
 
     return (
         vector_store, chroma_distance_fix, hardening, hardening_extra, recovery, quality_gate, final_gate,
@@ -129,6 +130,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
         phase16_contract_fix, ocr_state_fix, phase16_evidence_fix,
         version_transaction_fix, transaction_numpy_fix, failed_publication_cleanup,
         post_index_publication_contract, post_index_publication_contract_v2,
+        terminal_state_guard,
     )
 
 
