276f8f759fd1521817b934ecc24a44451d1f3eb6
mohanedpfe-rgb <mohanedpfe@gmail.com>
2026-09-14 13:54:15 +0100

    Wire ingestion cancel-flag lifecycle into runtime policy stack

--- rag_project/runtime.py
+++ rag_project/runtime.py
@@ -111,6 +111,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
     from rag_project.runtime_post_index_publication_contract import install as post_index_publication_contract
     from rag_project.runtime_post_index_publication_contract_v2 import install as post_index_publication_contract_v2
     from rag_project.runtime_terminal_state_guard import install as terminal_state_guard
+    from rag_project.runtime_cancel_flag_fix import install as cancel_flag_fix
 
     return (
         vector_store, chroma_distance_fix, hardening, hardening_extra, recovery, quality_gate, final_gate,
@@ -130,7 +131,7 @@ def _load_installers() -> tuple[Callable[[], None], ...]:
         phase16_contract_fix, ocr_state_fix, phase16_evidence_fix,
         version_transaction_fix, transaction_numpy_fix, failed_publication_cleanup,
         post_index_publication_contract, post_index_publication_contract_v2,
-        terminal_state_guard,
+        terminal_state_guard, cancel_flag_fix,
     )
 
 
